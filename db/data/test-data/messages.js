7ca5c303b4c71cc2606fa5215249306ddad03941
Nathan Pettit <nathanpettit@Nathans-MacBook-Pro-2.local>
2021-11-03 15:15:12 +0000

    populated users & messages tables with data

---
+++ db/data/test-data/messages.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    message_body: "I loved this game too!",
+    sent_by: "bainesface",
+    posted_in: 2,
+    created_at: new Date(1511354613389),
+  },
+  {
+    message_body: "My dog loved this game too!",
+    sent_by: "mallionaire",
+    posted_in: 3,
+    created_at: new Date(1610964545410),
+  },
+  {
+    message_body: "I didn't know dogs could play games",
+    sent_by: "philippaclaire9",
+    posted_in: 3,
+    created_at: new Date(1610964588110),
+  },
+  {
+    message_body: "EPIC board game!",
+    sent_by: "bainesface",
+    posted_in: 2,
+    created_at: new Date(1511354163389),
+  },
+  {
+    message_body:
+      "Now this is a story all about how, board games turned my life upside down",
+    sent_by: "mallionaire",
+    posted_in: 2,
+    created_at: new Date(1610965445410),
+  },
+  {
+    message_body:
+      "Not sure about dogs, but my cat likes to get involved with board games, the boxes are their particular favourite",
+    sent_by: "philippaclaire9",
+    posted_in: 3,
+    created_at: new Date(1616874588110),
+  },
+];
